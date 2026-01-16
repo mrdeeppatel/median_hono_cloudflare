c108f88bec8af3bebe571378125141ee53ecfa48
Deep Patel <mrvanpariya@gmail.com>
2026-01-16 15:09:10 +0530

    Create prisma schema and migrate database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,8 @@
 import { Hono } from 'hono'
-
+import { PrismaClient } from "./generated/prisma/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 const app = new Hono().basePath("/api/v1")
+
 
 app.post('/signup', (c) => {
   return c.text('Signup api!')
@@ -11,21 +13,22 @@ app.post('/signin', (c) => {
 })
 
 app.post('/blog', (c) => {
-return  c.json({
-  "msg":"uploading a blog"
-})
-})
-
-app.put('/blog',(c)=>{
   return c.json({
-    "msg":"Updating blog"
+    "msg": "uploading a blog"
   })
 })
 
-app.get('/blog/:id',(c)=>{
-return c.json({
-  "msg":"Getting all the blogs"
+app.put('/blog', (c) => {
+  return c.json({
+    "msg": "Updating blog"
+  })
 })
+
+app.get('/blog/:id', (c) => {
+  console.log("LOG -> " + c.req.query("we"))
+  return c.json({
+    "msg": "234"
+  })
 })
 
 export default app
