a5fdc591166c2b8457da51bb68c6486dc746e862
Deep Patel <mrvanpariya@gmail.com>
2026-01-20 19:30:26 +0530

    Add signip route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,63 @@
 import { Hono } from 'hono'
 import { PrismaClient } from "./generated/prisma/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
-const app = new Hono().basePath("/api/v1")
+// using hono jwt because there is a good change that jsonwebtoken does not work in the cloudflare environment
+import { decode, sign, verify } from 'hono/jwt';
 
 
-app.post('/signup', (c) => {
-  return c.text('Signup api!')
+interface Bindings {
+  DATABASE_URL: string,
+  JWTSECRET: string
+}
+// Hono Generic for Bindings 
+const app = new Hono<{ Bindings: Bindings }>().basePath("/api/v1")
+
+
+function getPrismaClient({ c }: { c: { env: { DATABASE_URL: string } } }) {
+
+  return new PrismaClient({
+    accelerateUrl: c.env.DATABASE_URL
+  }).$extends(withAccelerate())
+
+}
+
+app.post('/signup', async (c) => {
+
+  // console.log(c.env.DATABASE_URL)  
+
+  const body = await c.req.json()
+
+  const prisma = getPrismaClient({ c })
+
+  let user = null
+  try {
+    user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password,
+        name: body.name
+      }
+    })
+  } catch (e) {
+    c.status(409)
+    return c.json({
+      "MSG": "Error While creating user"
+    })
+  }
+
+  const payload = {
+    id: user.id,
+    email: user.email,
+    role: "user",
+    exp: Math.floor(Date.now() / 1000) + 60 * 10,
+  }
+
+  const token = await sign(payload, c.env.JWTSECRET)
+  
+  return c.json({
+    msg: "User Created",
+    jwt: token
+  })
 })
 
 app.post('/signin', (c) => {
@@ -24,10 +76,12 @@ app.put('/blog', (c) => {
   })
 })
 
-app.get('/blog/:id', (c) => {
-  console.log("LOG -> " + c.req.query("we"))
+app.get('/blog/:id', async (c) => {
+  console.log(await c.req.json())
+  console.log("LOG -> " + c.req.param("id"))
   return c.json({
-    "msg": "234"
+    "msg": "234",
+    "params_:id": c.req.param("id")
   })
 })
 
