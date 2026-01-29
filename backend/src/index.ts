bd0f440f81b08333d081fea8acc86274bdecbaf3
Deep Patel <mrvanpariya@gmail.com>
2026-01-29 15:05:33 +0530

    add auth middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,8 +9,12 @@ interface Bindings {
   DATABASE_URL: string,
   JWTSECRET: string
 }
+interface Variables {
+  email: unknown | string;
+}
+
 // Hono Generic for Bindings 
-const app = new Hono<{ Bindings: Bindings }>().basePath("/api/v1")
+const app = new Hono<{ Bindings: Bindings, Variables: Variables }>().basePath("/api/v1")
 
 
 function getPrismaClient({ c }: { c: { env: { DATABASE_URL: string } } }) {
@@ -39,10 +43,10 @@ app.post('/signup', async (c) => {
       }
     })
   } catch (e) {
-    c.status(409)
+    // c.status(409)
     return c.json({
       "MSG": "Error While creating user"
-    })
+    }, 409)
   }
 
   const payload = {
@@ -52,21 +56,87 @@ app.post('/signup', async (c) => {
     exp: Math.floor(Date.now() / 1000) + 60 * 10,
   }
 
-  const token = await sign(payload, c.env.JWTSECRET)
-  
+  const jwt = await sign(payload, c.env.JWTSECRET, "HS256")
+
   return c.json({
     msg: "User Created",
-    jwt: token
+    jwt
   })
 })
 
-app.post('/signin', (c) => {
-  return c.text('Signin api!')
+app.post('/signin', async (c) => {
+
+  const prisma = getPrismaClient({ c })
+  const body = await c.req.json();
+
+  // Checking payload for data
+  if (!body.email || !body.password) {
+    // c.status(409)
+    return c.json({
+      msg: "email or password in payload not found"
+    }, 409)
+  }
+
+  // Checking for user credentials
+  const user = await prisma.user.findUnique({
+    where: {
+      email: body.email,
+      password: body.password
+    }
+  })
+
+  if (!user) {
+    // c.status(409)
+    return c.json({
+      msg: "User credentials are not valid"
+    }, 409)
+  }
+
+  const payload = {
+    email: body.email
+  }
+
+  const jwt = await sign(payload, c.env.JWTSECRET, "HS256")
+
+  console.log(user)
+  return c.json({
+    msg: "Login Successful",
+    jwt
+  })
+})
+
+
+// Middleware
+// for api/v1/**
+app.use(async (c, next) => {
+
+  const header = c.req.header()
+  if (!header.jwt) {
+    return c.json({
+      msg: "Token not found"
+    }, 409)
+  }
+
+  const jwt = header.jwt
+
+  let result
+  try {
+    result = await verify(jwt, c.env.JWTSECRET, "HS256")
+  } catch (e) {
+    return c.json({
+      msg: "JWT is not valid"
+    }, 409)
+  }
+
+  c.set("email", result.email)
+  await next()
+
 })
 
 app.post('/blog', (c) => {
   return c.json({
-    "msg": "uploading a blog"
+    "msg": "uploading a blog",
+    "email": c.get("email")
   })
 })
 
@@ -77,7 +147,7 @@ app.put('/blog', (c) => {
 })
 
 app.get('/blog/:id', async (c) => {
-  console.log(await c.req.json())
+  // console.log(await c.req.json())
   console.log("LOG -> " + c.req.param("id"))
   return c.json({
     "msg": "234",
